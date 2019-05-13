f075c8ba7ed14b8753a6913b777cdf30e2081328
niw22 <ira.watt@newcastle.ac.uk>
2019-05-13 15:25:40 +0100

    First commit

---
+++ src/app/tab1/tab1.page.ts
@@ -0,0 +1,20 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-tab1',
+  templateUrl: 'tab1.page.html',
+  styleUrls: ['tab1.page.scss']
+})
+export class Tab1Page {
+    
+    
+  doRefresh(event) {
+    console.log('Begin async operation');
+
+    setTimeout(() => {
+      console.log('Async operation has ended');
+      event.target.complete();
+    }, 2000);
+  }
+    
+}
