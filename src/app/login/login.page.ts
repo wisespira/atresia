2350602bb67ab5ce9d7061b10ac698c3a2bd3b6c
niw22 <ira.watt@newcastle.ac.uk>
2019-05-28 09:53:16 +0100

    news

---
+++ src/app/login/login.page.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.page.html',
+  styleUrls: ['./login.page.scss'],
+})
+export class LoginPage implements OnInit {
+
+  username: string = "";
+  password: string = "";
+  constructor() { }
+
+  ngOnInit() {
+  }
+  
+  login(){
+      //const{username,password}= this
+      
+  }
+
+}
